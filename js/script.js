f501a046851ce59a5e376f88f064bec8ac3f2dfc
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-10-20 17:27:21 +0200

    BIGLIETTO TRENO | version 0.2
    improved js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ var eta = parseInt(prompt("Quanti anni hai ?"));
 console.log( "kilometri: " + km + ", Età: "+ eta);
 var totale = km * 0.21;
 
-if ( eta < "18"){
-  var under = totale * 0.8;
-  document.getElementById("totale").innerHTML = totale + " € - 20% di sconto = " + under + "€";
-} else if (eta > "65"){
-  var over = totale * 0.6;
-  document.getElementById("totale").innerHTML = totale + " € - 40% di sconto = " + over + "€";
-} else {
-  document.getElementById("totale").innerHTML = totale + "€";
-}
+
+  if ( eta < "18"){
+    var under = totale * 0.8;
+    document.getElementById("totale").innerHTML = totale + " € - 20% di sconto = " + under + "€";
+  } else if (eta > "65"){
+    var over = totale * 0.6;
+    document.getElementById("totale").innerHTML = totale + " € - 40% di sconto = " + over + "€";
+  } else  {
+    document.getElementById("totale").innerHTML = totale + "€";
+  }
