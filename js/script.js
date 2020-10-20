e6d6d4ddf1d39977917328235f696e571d504710
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-10-20 17:50:49 +0200

    BIGLIETTO TRENO | version 0.4
    - Improved JS
    -  Input control level:  You will only get a result if you enter a number in the data

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ var km = parseInt(prompt("Quanti Km devi fare ?"));
 var eta = parseInt(prompt("Quanti anni hai ?"));
 var totale = km * 0.21;
 
-
-  if ( eta < "18"){
+if (isNaN(km) || isNaN(eta)){
+  document.getElementById("totale").innerHTML = "Inserisci correttamente i dati";
+} else {
+  if ( eta < 18){
     var under = totale * 0.8;
     document.getElementById("totale").innerHTML = totale + " € - 20% di sconto = " + under + "€";
-  } else if (eta > "65"){
+  } else if (eta > 65){
     var over = totale * 0.6;
     document.getElementById("totale").innerHTML = totale + " € - 40% di sconto = " + over + "€";
   } else  {
     document.getElementById("totale").innerHTML = totale + "€";
   }
+}
