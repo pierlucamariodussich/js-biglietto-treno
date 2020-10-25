3bba970e169229b945dbef3d3f5dac19e5ed24db
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-10-25 17:53:32 +0100

    BIGLIETTO TRENO | version 1.0
    > form js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,59 @@
 
-var km = parseInt(prompt("Quanti Km devi fare ?"));
-var eta = parseInt(prompt("Quanti anni hai ?"));
+var name = document.getElementById("nome").value
+var surname = document.getElementById("cognome").value
+var km = document.getElementById("km").value
+var eta = document.getElementById("eta").value
 var totale = km * 0.21;
+var btnSend = document.getElementById("send");
+var btnReset = document.getElementById("reset")
+var carriageEL = (Math.floor(Math.random()* 10) + 1);
+var cpEl = (Math.floor(Math.random()* 100000) + 1);
+var boxT =document.getElementById("boxT")
 
-if (isNaN(km) || isNaN(eta)){
-  document.getElementById("totale").innerHTML = "Inserisci correttamente i dati";
-} else {
-  if ( eta < 18){
-    var under = totale * 0.8;
-    document.getElementById("totale").innerHTML = totale + " € - 20% di sconto = " + under + "€";
-  } else if (eta > 65){
-    var over = totale * 0.6;
-    document.getElementById("totale").innerHTML = totale + " € - 40% di sconto = " + over + "€";
-  } else  {
-    document.getElementById("totale").innerHTML = totale + "€";
+btnSend.addEventListener("click",
+  function (){
+
+  if (isNaN(km) || isNaN(eta)){
+    document.getElementById("totale").innerHTML = "Inserisci correttamente i dati";
+  } else {
+    if ( eta < 18){
+      var under = Math.floor(totale * 0.8);
+      document.getElementById("totale").innerHTML =  under + "€";
+      discountEl = "-20%";
+      document.getElementById("discount").innerHTML =  discountEl;
+      document.getElementById("user").innerHTML =  name + " " + surname;
+      document.getElementById("carriage").innerHTML = carriageEL;
+      document.getElementById("cp").innerHTML =  cpEl;
+      boxT.className -= "hidden";
+    } else if (eta > 65){
+      var over = Math.floor(totale * 0.6);
+      document.getElementById("totale").innerHTML =  over + "€";
+      discountEl = "-40%";
+      document.getElementById("discount").innerHTML =  discountEl;
+      document.getElementById("user").innerHTML =  name + " " + surname;
+      document.getElementById("carriage").innerHTML = carriageEL;
+      document.getElementById("cp").innerHTML =  cpEl;
+      boxT.className -= "hidden";
+    } else  {
+      document.getElementById("totale").innerHTML = totale + "€";
+      discountEl = "-";
+      document.getElementById("discount").innerHTML =  discountEl;
+      document.getElementById("user").innerHTML =  name + " " + surname;
+      document.getElementById("carriage").innerHTML = carriageEL;
+      document.getElementById("cp").innerHTML =  cpEl;
+      boxT.className -= "hidden";
+    }
   }
-}
+
+})
+
+btnReset.addEventListener("click",
+  function (){
+  document.getElementById("nome").value =  "";
+  document.getElementById("cognome").value =  "";
+  document.getElementById("km").value =  "";
+  document.getElementById("eta").value =  "";
+  boxT.className = "biglietto hidden";
+
+
+})
